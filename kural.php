ea12b52a49c657c61728ef0c27b7b80895366fdb
Kesavan Muthuvel <kesavan.muthuvel@directenergy.com>
2022-12-13 14:23:17 -0600

    tuned

--- kural.php
+++ kural.php
@@ -85,16 +85,5 @@ class பாடல் {
         return array_product($isSuccess);
     }
 }
-# Thou a beautiful poem π , just at look - tiggers 
-# hot love, you never change great constant ! 
-$மாதிரி = " கவிதை நீ ஆகிறாய் பை, கண்டதும்         
-            காதல்தூண்டிவிடும் மாறா மாறிலிநீயடி";
-$பாடல் = new பாடல்($மாதிரி);
-$பாடல்->பாடல்_பையமா();
 
-$குறள்கள் = file('குறள்.txt');       
-foreach($குறள்கள் as $குறள் ){
-    $பாடல் = new பாடல்($குறள்);
-    $பாடல்->பாடல்_பையமா();
-}
 ?>
